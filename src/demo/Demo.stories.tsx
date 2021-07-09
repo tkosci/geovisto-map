4e02b8afd7a6ba48a110964184fca0c51d459c3c
Jiri Hynek <xhynek@gmail.com>
2021-07-09 13:54:16 +0200

    Feature #9: empty config restored

--- src/demo/Demo.stories.tsx
+++ src/demo/Demo.stories.tsx
@@ -59,7 +59,7 @@ class Demo extends Component {
     const jsonData = require("/static/data/demo1.json");
 
     // // implicit config
-    const jsonConfig = require("/static/config/config-demo1.json");
+    const jsonConfig = require("/static/config/config.json");
 
     // reference to the rendered map
     this.map = React.createRef();
@@ -151,7 +151,7 @@ class Demo extends Component {
 
       // process config json
       if (!document.getElementById(C_ID_check_config).checked || config.json == undefined) {
-        config.json = require("/static/config/config-demo1.json");
+        config.json = require("/static/config/config.json");
       }
 
       // update state
