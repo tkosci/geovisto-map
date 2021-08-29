706429090c33ac88fb78a18a3f0763b9fb1c97f9
Andrej Tlcina <attanox@gmail.com>
2021-08-29 16:27:20 +0200

    Feature #15: add authoring layer

---
+++ src/tools/layers/drawing/model/types/tool/IDrawingLayerToolDefaults.ts
@@ -0,0 +1,46 @@
+// Geovisto core
+import { ILayerToolDefaults } from "../../../../../../index.core";
+import IMapFormInput from "../../../../../../model/types/inputs/IMapFormInput";
+
+type InputName =
+  | "idKey"
+  | "identifier"
+  | "description"
+  | "strokeThickness"
+  | "search"
+  | "searchForArea"
+  | "adminLevel"
+  | "iconUrl"
+  | "dataFilterKey"
+  | "dataFilterValue"
+  | "brushSize"
+  | "customToleranceValue"
+  | "iconAnchor"
+  | "customToleranceCheck"
+  | "changeConnect"
+  | "intersectActivated"
+  | "searchConnect"
+  | "highQuality";
+
+export type MappingModel = {
+  [key in InputName]: {
+    props: {
+      name: string;
+      label?: string;
+    };
+    input: (props: any) => IMapFormInput;
+  };
+};
+
+/**
+ * This interface provides functions which return the default state values.
+ *
+ * @author Jiri Hynek
+ */
+interface IDrawingLayerToolDefaults extends ILayerToolDefaults {
+  getType(): string;
+  getLayerName(): string;
+  getLabel(): string;
+  getIcon(): string;
+}
+export default IDrawingLayerToolDefaults;
